189d29d9b6b51c56827892cff27d531ed2eacb50
Patryk Kalinowski <patryk@kalinowski.dev>
2025-04-18 15:15:41 +0200

    rpc: refactor attestation middleware to separate pre and post processing, add user data generation

--- rpc/internal/attestation/middleware.go
+++ rpc/internal/attestation/middleware.go
@@ -1,15 +1,18 @@
 package attestation
 
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
 	"github.com/0xsequence/identity-instrument/o11y"
 	"github.com/0xsequence/identity-instrument/proto"
 	"github.com/0xsequence/nitrocontrol/enclave"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 // Middleware is an HTTP middleware that issues an attestation document request to the enclave's NSM.
@@ -19,40 +22,82 @@ import (
 // If the HTTP request includes an X-Attestation-Nonce header, its value is sent to the NSM and included in
 // the final attestation document.
 func Middleware(enc *enclave.Enclave) func(http.Handler) http.Handler {
-	runMiddleware := func(w http.ResponseWriter, r *http.Request) (ctx context.Context, cancelFunc func() error, err error) {
+	runPreMiddleware := func(r *http.Request) (ctx context.Context, cancelFunc func() error, err error) {
 		ctx, span := o11y.Trace(r.Context(), "attestation.Middleware")
 		defer func() {
 			span.RecordError(err)
 			span.End()
 		}()
 
-		var nonce []byte
-		if nonceVal := r.Header.Get("X-Attestation-Nonce"); nonceVal != "" {
-			nonceVal = strings.TrimSpace(nonceVal)
-			if len(nonceVal) > 32 {
-				return nil, nil, fmt.Errorf("X-Attestation-Nonce value cannot be longer than 32")
-			}
-			nonce = []byte(nonceVal)
-		}
-
-		att, err := enc.GetAttestation(ctx, nonce)
+		att, err := enc.GetAttestation(ctx, nil, nil)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		w.Header().Set("X-Attestation-Document", base64.StdEncoding.EncodeToString(att.Document()))
 		return context.WithValue(r.Context(), contextKey, att), att.Close, nil
+	}
+
+	runPostMiddleware := func(w http.ResponseWriter, r *http.Request, body []byte, nonce []byte) (err error) {
+		ctx, span := o11y.Trace(r.Context(), "attestation.Middleware")
+		defer func() {
+			span.RecordError(err)
+			span.End()
+		}()
+
+		userData, err := generateUserData(r, body)
+		if err != nil {
+			return err
+		}
+
+		att, err := enc.GetAttestation(ctx, nonce, userData)
+		if err != nil {
+			return err
+		}
+		defer att.Close()
+
+		w.Header().Set("X-Attestation-Document", base64.StdEncoding.EncodeToString(att.Document()))
+		return nil
 	}
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx, cancel, err := runMiddleware(w, r)
+			reqBody, _ := io.ReadAll(r.Body)
+			r.Body = io.NopCloser(bytes.NewBuffer(reqBody))
+
+			var nonce []byte
+			if nonceVal := r.Header.Get("X-Attestation-Nonce"); nonceVal != "" {
+				nonceVal = strings.TrimSpace(nonceVal)
+				if len(nonceVal) > 32 {
+					proto.RespondWithError(w, fmt.Errorf("X-Attestation-Nonce value cannot be longer than 32"))
+					return
+				}
+				nonce = []byte(nonceVal)
+			}
+
+			ctx, cancel, err := runPreMiddleware(r)
 			if err != nil {
 				proto.RespondWithError(w, err)
 				return
 			}
 			defer cancel()
-			next.ServeHTTP(w, r.WithContext(ctx))
+
+			var body bytes.Buffer
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			ww.Tee(&body)
+			ww.Discard()
+
+			next.ServeHTTP(ww, r.WithContext(ctx))
+
+			r.Body = io.NopCloser(bytes.NewBuffer(reqBody))
+			if err := runPostMiddleware(ww, r, body.Bytes(), nonce); err != nil {
+				proto.RespondWithError(w, err)
+				return
+			}
+
+			w.WriteHeader(ww.Status())
+			if _, err := body.WriteTo(w); err != nil {
+				proto.RespondWithError(w, err)
+			}
 		})
 	}
 }
---
+++ rpc/internal/attestation/userdata.go
@@ -0,0 +1,32 @@
+package attestation
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func generateUserData(r *http.Request, resBody []byte) ([]byte, error) {
+	hasher := sha256.New()
+	hasher.Write([]byte(r.Method + " " + r.URL.Path + "\n"))
+
+	var reqBody []byte
+	if r.Body != nil {
+		reqBody, _ = io.ReadAll(r.Body)
+		r.Body = io.NopCloser(bytes.NewBuffer(reqBody))
+		hasher.Write(reqBody)
+	}
+	hasher.Write([]byte("\n"))
+	hasher.Write(resBody)
+	hash := hasher.Sum(nil)
+
+	fmt.Println(r.Method + " " + r.URL.Path)
+	fmt.Println(string(reqBody))
+	fmt.Println(string(resBody))
+
+	userData := "Sequence/1:" + base64.StdEncoding.EncodeToString(hash)
+	return []byte(userData), nil
+}
