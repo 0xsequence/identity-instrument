7f6d126466a22f7e23879c1228d97161f573d8df
Patryk Kalinowski <patryk@kalinowski.dev>
2026-01-22 11:56:19 +0100

    proto: improve schema organisation

--- proto/proto.go
+++ proto/proto.go
@@ -1,8 +1,8 @@
 // Server
-//go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=identity-instrument.ridl -target=golang -pkg=proto -server -client -out=./identity-instrument.gen.go
+//go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=./schema/identity-instrument.ridl -target=golang -pkg=proto -server -client -out=./identity-instrument.gen.go
 
 // Clients
-//go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=identity-instrument.ridl -target=golang -pkg=proto -client -out=./clients/identity-instrument.gen.go
-//go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=identity-instrument.ridl -target=typescript -client -out=./clients/identity-instrument.gen.ts
+//go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=./schema/identity-instrument.ridl -target=golang -pkg=proto -client -out=./clients/identity-instrument.gen.go
+//go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=./schema/identity-instrument.ridl -target=typescript -client -out=./clients/identity-instrument.gen.ts
 
 package proto
